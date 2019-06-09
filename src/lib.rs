4b0bcd661f4b4d4defd1dd9424de4896c511819b
toshokan <toshokan@shojigate.net>
2019-06-09 15:05:56 -0400

    Document crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # Rot
+//! Strongly-typed ascii character rotations in the style of Caesar/Rot/Vigenère ciphers.
+//! This is only meant to be some fun with type-level integers in Rust.
+//! *Please do not use this as an encryption scheme* as it is *laughably* weak.
+
 use typenum::{Integer};
 use typenum::consts::{Z0, P26};
 use typenum::type_operators::{Same};
@@ -7,6 +12,82 @@ use std::ops::{Add, Rem};
 
 use std::fmt;
 
+/// A wrapper over Ascii `u8` which rotates alphabetic characters by `N`.
+/// This is essentially a per-character Caesar cipher with a shift of `N`.
+/// A collection of this type may also be used to implement a strongly-typed Vigenère cipher.
+/// An interesting characteristic is that this type can only be converted back to
+/// a raw `u8` when it has been rotated back into place (i.e `N % 26 == 0`),
+/// otherwise it is a compile-time type error to do so.
+/// It is also type-parameterized over the rotation length to prevent
+/// partial/improper ciphers at compile-time.
+/// This makes it impossible to use a byte rotated right by 12 when a type contract
+/// requires a byte rotated left by 13, for example.
+///
+/// # Examples
+/// ## Rotate some bytes `u8` by 7 positions to the right.
+/// ```
+/// use rot::{Rotate, RotU8};
+/// use typenum::consts::P7;
+/// let s = "a b c";
+/// let r : Vec<RotU8<P7>> = s.bytes().rotate_by::<P7>().collect();
+/// ```
+///
+/// ## Try to incorrectly read a rotated byte
+/// ```compile_fail
+/// use rot::{Rotate, RotU8};
+/// use typenum::consts::P1;
+/// let c = 'a' as u8;
+/// let rotated: RotU8<P1> = c.into();
+/// let contents: u8 = rotated.into();  // This will fail to type check!
+/// ```
+///
+/// ## Perform no-op rotations
+/// ```
+/// use rot::{Rotate, RotU8};
+/// use typenum::consts::{Z0, P26};
+/// let c = 'a' as u8;
+/// let rotated_by_zero: RotU8<Z0> = c.into();
+/// let rotated_by_26: RotU8<P26> = c.into();
+/// let contents_0: u8 = rotated_by_zero.into();  // This works, `c` wasn't rotated.
+/// assert_eq!(contents_0, 'a' as u8);
+/// // This works, `c` was rotated back to the start point.
+/// let contents_26: u8 = rotated_by_26.into();
+/// assert_eq!(contents_26, 'a' as u8);
+/// ```
+///
+/// ## Type safe signatures
+/// ```
+/// use typenum::{Integer};
+/// use typenum::consts::{Z0, P1, N1};
+/// use rot::RotU8;
+///
+/// // This function cannot secretly rotate the byte (unless it rotates it back!)
+/// // This would be useful in a trait definition to enforce a stronger contract.
+/// fn should_not_rotate<N: Integer>(rc: RotU8<N>) -> RotU8<N> {
+///     unimplemented!();
+/// }
+/// ```
+/// 
+/// ```compile_fail
+/// use typenum::{Integer};
+/// use typenum::consts::{Z0, P1, N1};
+/// use rot::RotU8;
+///
+/// fn evil_function<N: Integer>(rc: RotU8<N>) -> RotU8<N> {
+///     rc.rotate_by::<P1>() // Compile-time error!
+///     // ^ this has type RotU8<N+1>
+/// }
+/// ```
+/// 
+/// ```
+/// use typenum::{Integer};
+/// use typenum::consts::{Z0, P1, N1};
+/// use rot::RotU8;
+///
+/// fn not_so_evil_function<N: Integer>(rc: RotU8<N>) -> RotU8<N> {
+///     rc.rotate_by::<Z0>() // Works fine! Adding zero (or a multiple of 26) doesn't rotate.
+/// }
+/// ````
 #[derive(Clone, Copy)]
 pub struct RotU8<N: Integer>(u8, PhantomData<N>);
 
@@ -18,25 +99,37 @@ impl<N: Integer> fmt::Display for RotU8<N> {
 
 impl<N: Integer> fmt::Debug for RotU8<N> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "RotU8<{}>({})", N::to_i8(), self.0 as char)
+        write!(f, "RotU8<{}>({})", N::to_i8(), &self.0)
     }
 }
 
 impl<N> RotU8<N>
 where N: Integer {
+    /// Create a new `RotU8<N>` from a pre-rotated character byte.
     fn new(rotated: u8) -> Self {
         Self(rotated, PhantomData)
     }
-    
+
+    /// Wrap an offset character byte (in the positive _or_ negative directions)
+    /// and ensure it is in the range `[0, 26)`
     fn wrapping_rotate_mod26(c: i8) -> u8 {
         ((((c + N::to_i8()) % 26) + 26) % 26) as u8
     }
-    
+
+    /// Rotate a character relative to a start point for a 26 character range.
+    /// If rotating a lowercase ascii letter, the range starts at `'a'`,
+    /// otherwise it starts at `'A'`.
     fn rotate_relative(c: u8, zero_point: u8) -> u8 {
         let offset = c - zero_point;
         Self::wrapping_rotate_mod26(offset as i8) + zero_point
     }
-    
+
+    /// Rotate the raw ascii character `ch` `N` positions,
+    /// _if is an alphabetic character_,
+    /// otherwise do not change it.
+    /// If `N` is positive, this performs a shift to the right.
+    /// If `N` is negative, this performs a shift to the left.
+    /// A zero value for `N` is identity.
     fn rotate(ch: u8) -> u8 {
         let c = ch as i8;
         match ch as char {
@@ -50,6 +143,7 @@ where N: Integer {
         }
     }
 
+    // Chain a rotation by `N` with a rotation by `M`.
     pub fn rotate_by<M>(self) -> RotU8<Sum<M, N>>
     where
         M: Add<N> + Integer,
@@ -71,11 +165,13 @@ where
     }
 }
 
+/// Wraps a raw character/byte iterator and rotates each item.
 pub struct RotIter<N: Integer, I: Iterator> {
     iter: I,
     _p: PhantomData<N>
 }
 
+/// Rotate `Self::Item` by `N`.
 pub trait Rotate
 where
     Self: Iterator + Sized
